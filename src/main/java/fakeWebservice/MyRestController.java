8ca4bc7feb735b5f5cc0ba87715e535e410efb94
a.piergiacomi <a.piergiacomi@ocado.com>
2016-03-22 11:00:25 +0000

    clean up

--- src/main/java/fakeWebservice/MyRestController.java
+++ src/main/java/fakeWebservice/MyRestController.java
@@ -2,16 +2,15 @@ package fakeWebservice;
 
 import java.io.IOException;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class Rest {
+public class MyRestController {
 
 	@RequestMapping(value = "/users/1")
-	public @ResponseBody String tags() throws IOException {
+	public @ResponseBody String method1() throws IOException {
 		return "1";
 	}
 
